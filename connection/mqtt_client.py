3c0efe51df91a776f8823218ed8e75a080be3ac1
noefforted <ahmadopa73@gmail.com>
2024-10-24 16:22:38 +0000

    finsih

--- connection/mqtt_client.py
+++ connection/mqtt_client.py
@@ -1,11 +1,15 @@
 import asyncio
 import json
+import logging
 from paho.mqtt import client as mqtt_client
 from service.pzem_service import PzemService
 from model.pzem_data import PzemData
 
+# Konfigurasi logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 broker = 'localhost'
-port = 1883
+port = 50013
 topic = "sensor/pzem"
 client_id = "fastapi-client"
 pzem_service = PzemService()
@@ -19,21 +23,19 @@ def connect_mqtt():
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
-        print("Terhubung ke broker MQTT")
+        logging.info("Terhubung ke broker MQTT")
         client.subscribe(topic)
     else:
-        print(f"Failed to connect, return code {rc}")
+        logging.error(f"Failed to connect, return code {rc}")
 
 def on_message(client, userdata, msg):
     try:
-        value = msg.payload.decode()
-        print(f"Pesan diterima dari `{msg.topic}`: {value}")
-        data = json.loads(value)
-        
+        logging.info(f"Pesan diterima dari `{msg.topic}`: {msg.payload.decode()}")
+        data = json.loads(msg.payload.decode())
         # Validasi dan parsing data JSON
         pzem_data = PzemData(**data)
-
+        logging.info(f"Data Masuk: {pzem_data}")
         # Menggunakan asyncio untuk memanggil fungsi async di callback sync
         asyncio.run(pzem_service.save_data(pzem_data))
     except Exception as e:
-        print(f"Error saat memproses pesan: {e}")
+        logging.error(f"Error saat memproses pesan: {e}")
